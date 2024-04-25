1fb42b379050c45f36152e4c7807791911d6bd48
구관조 <19607901-kangteato@users.noreply.replit.com>
2024-04-25 08:17:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("안녕하세요, 밴드네임 제작소입니다.")
+#2. Ask the user for the city that they grew up in.
+city = input("당신이 태어난 곳은 어디인가요?\n")
+#3. Ask the user for the name of a pet.
+pet = input("당신이 키우는 애완동물의 이름은?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("당신의 밴드네임은 " + city + pet + "입니다.")
+#5. Make sure the input cursor shows on a new line:
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
